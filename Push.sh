48757f1d80c153f1e9a0a1fb233a31e0e0e6f5e8
thephuoc160 <thephuoc@outlook.com>
2025-06-04 09:48:06 +0700

    04-06-2025 new add file with random number

--- Push.sh
+++ Push.sh
@@ -9,8 +9,8 @@ git commit -m "Add Text.txt with random number"
 
 git add .
 
-# git remote add origin https://github.com/thephuoc/GitHub_SSH_2.git
-git remote add origin git@hub2:thephuoc160/Git_Push_SSH.git
+# git remote add origin https://thephuoc160@github.com/thephuoc160/Git_Push_SSH.git
+git remote add origin git@hub1:thephuoc/GitHub_SSH_2.git
 
 git commit -m "$(date +%d-%m-%Y) new add file with random number"
 
@@ -36,8 +36,11 @@ fi
 # Read the contents of the `text.txt` file
 text=$(cat "$current_directory/text.txt")
 
-# Add the body of the `text.txt` file to the `index.html` file
-echo "$text" >> "$current_directory/index.html"
+# Đọc từng dòng trong text.txt và thêm \n vào cuối từng dòng
+text=$(awk '{printf "%s\\\\n", $0}' "$current_directory/text.txt")
+
+# Ghi đè dòng 168 trong index.html bằng dòng mới
+sed -i "168s|.*|const numbersStringFromFileEnd = \"$text\";|" "$current_directory/index.html"
 
 # Save the changes to the `index.html` file
 git add "$current_directory/index.html"
@@ -46,3 +49,4 @@ git add "$current_directory/index.html"
 git commit -m "$(date +%d-%m-%Y) Auto-added body of `text.txt` file to `index.html` file."
 
 git push -u origin master --force
+
